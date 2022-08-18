cddc81fd3e4502fc97fe40b81fed2572751e5e8d
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-18 17:21:06 -0300

    requisito 5 e 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          Planets.map((cadaPlaneta) => (
+            <PlanetCard
+              planetName={
+                cadaPlaneta.name
+              }
+              planetImage={
+                cadaPlaneta.image
+              }
+              key={
+                cadaPlaneta.name
+              }
+            />
+          ))
+        }
       </div>
     );
   }
 }
+
 export default SolarSystem;
