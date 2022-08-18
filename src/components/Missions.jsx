258187fe0a3988099984844ae6774694e2c7b330
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2022-08-18 18:07:04 -0300

    requisito 9 e 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {
+          missions.map((cadaMissao) => (
+            <MissionCard
+              name={
+                cadaMissao.name
+              }
+              year={
+                cadaMissao.year
+              }
+              country={
+                cadaMissao.country
+              }
+              destination={
+                cadaMissao.destination
+              }
+              key={ cadaMissao.name }
+            />
+          ))
+        }
       </div>
     );
   }
